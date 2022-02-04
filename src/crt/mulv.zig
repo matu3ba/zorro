7c8860250486b2b1a0d3b533e7ec735c253c715d
Jan Philipp Hafer <jan.hafer@rwth-aachen.de>
2022-02-04 13:54:02 +0100

    comments in mulv and fast approx with slow fallback

--- src/crt/mulv.zig
+++ src/crt/mulv.zig
@@ -5,17 +5,20 @@ const math = std.math;
 // mulv - multiplication oVerflow
 // * @panic, if result can not be represented
 // - mulvXi_genericFast for generic performance implementation
-//   * target must be able to represent result in register
+//   * auto-selected, if target can represent result in register
 // - mulvXi_genericSmall otherwise or on ReleaseSmall or user-setting
+//   * otherwise selected
+//
+// Portable speedup method at cost of higher binary size
 // - mulvXi_genericApprox for approximation based on number of leading zeroes
 //   based on Hacker's Delight, chapter 2–13 Overflow Detection, section Multiplication
-// TODO benchmark, if approximation is more useful than simpler division approach for embedded
-// TODO also measure used binary space for embedded
+// - mulvXi_genericMostlyFast for the approximation with mulvXi_genericSmall as
+//   slow fallback
+// TODO measure binary space
+// TODO benchmark mulvXi_genericMostlyFast
 
-// assume
-// 1. CPUs with higher integer range have intrinsics,
-// 2. target architectures can not or does not want tot use bigger number ranges
-// 2. fast approximation
+// fast approximation
+// assume: target architectures can not or does not want to use bigger number ranges
 fn mulvXi_genericApprox(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
     return struct {
         fn f(a: ST, b: ST) callconv(.C) ST {
@@ -49,6 +52,7 @@ fn mulvXi_genericApprox(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
     }.f;
 }
 
+// slower but portable routine
 fn mulvXi_genericSmall(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
     return struct {
         fn f(a: ST, b: ST) callconv(.C) ST {
@@ -57,6 +61,47 @@ fn mulvXi_genericSmall(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
                 return -5;
             }
             return @truncate(ST, res);
+        }
+    }.f;
+}
+
+// fast approximation with slow path as fallback
+fn mulvXi_genericMostlyFast(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
+    return struct {
+        fn f(a: ST, b: ST) callconv(.C) ST {
+            const clzfn = switch (ST) {
+                i32 => clz.__clzsi2,
+                i64 => clz.__clzdi2,
+                i128 => clz.__clzti2,
+                else => unreachable,
+            };
+            const m: i32 = clzfn(a) + clzfn(~a);
+            const n: i32 = clzfn(b) + clzfn(~b);
+            const sum: i32 = m + n;
+            // 1. no overflow, if s^{S_A+S_B} < 2^{n-1}
+            // => S_A + S_B < n-1 return a*b;
+            if (sum >= 34) return a * b;
+            // 2. guaranteed overflow
+            if (sum <= 31) return -5;
+            // 3. S_A + S_B = n => 2^{n-2} <= |P| <= 2^n
+            // overflow may occur, but magnitude does not exceed 2^n
+            if (sum == 33) {
+                if (m ^ n ^ (m ^ n) < 0) return -5;
+                return a * b;
+            }
+            // fallback to slow method for case `sum == 32`
+            // hardware support is required for accurate and speedy detection,
+            // see "Integer Multipliers with Overflow Detection by Gok" et al.
+            // p_n and p_{n-1} are not available without space extension
+            // and wrapping can not store the necessary information.
+            {
+                @setCold(true);
+                var res: ST = a *% b;
+                if (a != 0 and @divTrunc(res, a) != b) {
+                    return -5;
+                }
+                return res;
+            }
         }
     }.f;
 }
@@ -89,8 +134,7 @@ pub const mulvsi_genericSmall = mulvXi_genericSmall(i32);
 pub const mulvdi_genericSmall = mulvXi_genericSmall(i64);
 pub const mulvti_genericSmall = mulvXi_genericSmall(i128);
 
-// TODO comptime logic to ensure selection is actually fast
-// (register size available on target)
+// TODO comptime logic to ensure register size available on target
 pub const mulvsi3genFast = mulvXi_genericFast(i32);
 pub const mulvdi3genFast = mulvXi_genericFast(i64);
 pub const mulvti3genFast = mulvXi_genericFast(i128);
