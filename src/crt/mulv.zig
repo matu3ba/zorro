e6dc7b5e1985f1db8ff19c5feedc70d96c30d891
Jan Philipp Hafer <jan.hafer@rwth-aachen.de>
2022-02-04 18:10:06 +0100

    single branch check for signed multiplication overflow

--- src/crt/mulv.zig
+++ src/crt/mulv.zig
@@ -1,9 +1,11 @@
 const clz = @import("count0bits.zig");
 const std = @import("std");
 const math = std.math;
+const builtin = @import("builtin");
 
 // mulv - multiplication oVerflow
 // * @panic, if result can not be represented
+// * assume usize shows available register size for usage
 // - mulvXi_genericFast for generic performance implementation
 //   * auto-selected, if target can represent result in register
 // - mulvXi_genericSmall otherwise or on ReleaseSmall or user-setting
@@ -19,122 +21,132 @@ const math = std.math;
 
 // fast approximation
 // assume: target architectures can not or does not want to use bigger number ranges
-fn mulvXi_genericApprox(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
-    return struct {
-        fn f(a: ST, b: ST) callconv(.C) ST {
-            const clzfn = switch (ST) {
-                i32 => clz.__clzsi2,
-                i64 => clz.__clzdi2,
-                i128 => clz.__clzti2,
-                else => unreachable,
-            };
-            const m: i32 = clzfn(a) + clzfn(~a);
-            const n: i32 = clzfn(b) + clzfn(~b);
-            const sum: i32 = m + n;
-            // 1. no overflow, if s^{S_A+S_B} < 2^{n-1}
-            // => S_A + S_B < n-1 return a*b;
-            if (sum >= 34) return a * b;
-            // 2. guaranteed overflow
-            if (sum <= 31) return -5;
-            // 3. S_A + S_B = n => 2^{n-2} <= |P| <= 2^n
-            // overflow may occur, but magnitude does not exceed 2^n
-            if (sum == 33) {
-                if (m ^ n ^ (m ^ n) < 0) return -5;
-                return a * b;
-            }
-            // return overflow in all cases as safe over-approximation
-            // hardware support is required for accurate and speedy detection,
-            // see "Integer Multipliers with Overflow Detection by Gok" et al.
-            // p_n and p_{n-1} are not available without space extension
-            // and wrapping can not store the necessary information.
-            return -5;
-        }
-    }.f;
+inline fn mulvXi_genericApprox(comptime ST: type, a: ST, b: ST) ST {
+    @setRuntimeSafety(builtin.is_test);
+    const clzfn = switch (ST) {
+        i32 => clz.__clzsi2,
+        i64 => clz.__clzdi2,
+        i128 => clz.__clzti2,
+        else => unreachable,
+    };
+    const m: i32 = clzfn(a) + clzfn(~a);
+    const n: i32 = clzfn(b) + clzfn(~b);
+    const sum: i32 = m + n;
+    // 1. no overflow, if s^{S_A+S_B} < 2^{n-1}
+    // => S_A + S_B < n-1 return a*b;
+    if (sum >= 34) return a * b;
+    // 2. guaranteed overflow
+    if (sum <= 31) return -5;
+    // 3. S_A + S_B = n => 2^{n-2} <= |P| <= 2^n
+    // overflow may occur, but magnitude does not exceed 2^n
+    if (sum == 33) {
+        if (m ^ n ^ (m ^ n) < 0) return -5;
+        return a * b;
+    }
+    // return overflow in all cases as safe over-approximation
+    // hardware support is required for accurate and speedy detection,
+    // see "Integer Multipliers with Overflow Detection by Gok" et al.
+    // p_n and p_{n-1} are not available without space extension
+    // and wrapping can not store the necessary information.
+    return -5;
 }
 
 // slower but portable routine
-fn mulvXi_genericSmall(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
-    return struct {
-        fn f(a: ST, b: ST) callconv(.C) ST {
-            var res: ST = a *% b;
-            if (a != 0 and @divTrunc(res, a) != b) {
-                return -5;
-            }
-            return @truncate(ST, res);
-        }
-    }.f;
+inline fn mulvXi_genericSmall(comptime ST: type, a: ST, b: ST) ST {
+    @setRuntimeSafety(builtin.is_test);
+    const min = math.minInt(ST);
+    var res: ST = a *% b;
+    // Hacker's Delight section Overflow subsection Multiplication
+    // case a=-2^{31}, b=-1 problem, because
+    // on some machines a*b = -2^{31} with overflow
+    // Then -2^{31}/-1 overflows and any result is possible.
+    // => check with a<0 and b=-2^{31}
+    if ((a < 0 and b == min) or (a != 0 and res / a != b))
+        return -5;
+    return @truncate(ST, res);
 }
 
 // fast approximation with slow path as fallback
-fn mulvXi_genericMostlyFast(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
-    return struct {
-        fn f(a: ST, b: ST) callconv(.C) ST {
-            const clzfn = switch (ST) {
-                i32 => clz.__clzsi2,
-                i64 => clz.__clzdi2,
-                i128 => clz.__clzti2,
-                else => unreachable,
-            };
-            const m: i32 = clzfn(a) + clzfn(~a);
-            const n: i32 = clzfn(b) + clzfn(~b);
-            const sum: i32 = m + n;
-            // 1. no overflow, if s^{S_A+S_B} < 2^{n-1}
-            // => S_A + S_B < n-1 return a*b;
-            if (sum >= 34) return a * b;
-            // 2. guaranteed overflow
-            if (sum <= 31) return -5;
-            // 3. S_A + S_B = n => 2^{n-2} <= |P| <= 2^n
-            // overflow may occur, but magnitude does not exceed 2^n
-            if (sum == 33) {
-                if (m ^ n ^ (m ^ n) < 0) return -5;
-                return a * b;
-            }
-            // fallback to slow method for case `sum == 32`
-            // hardware support is required for accurate and speedy detection,
-            // see "Integer Multipliers with Overflow Detection by Gok" et al.
-            // p_n and p_{n-1} are not available without space extension
-            // and wrapping can not store the necessary information.
-            {
-                @setCold(true);
-                var res: ST = a *% b;
-                if (a != 0 and @divTrunc(res, a) != b) {
-                    return -5;
-                }
-                return res;
-            }
-        }
-    }.f;
+inline fn mulvXi_genericMostlyFast(comptime ST: type, a: ST, b: ST) ST {
+    @setRuntimeSafety(builtin.is_test);
+    const clzfn = switch (ST) {
+        i32 => clz.__clzsi2,
+        i64 => clz.__clzdi2,
+        i128 => clz.__clzti2,
+        else => unreachable,
+    };
+    const m: i32 = clzfn(a) + clzfn(~a);
+    const n: i32 = clzfn(b) + clzfn(~b);
+    const sum: i32 = m + n;
+    // 1. no overflow, if s^{S_A+S_B} < 2^{n-1}
+    // => S_A + S_B < n-1 return a*b;
+    if (sum >= 34) return a * b;
+    // 2. guaranteed overflow
+    if (sum <= 31) return -5;
+    // 3. S_A + S_B = n => 2^{n-2} <= |P| <= 2^n
+    // overflow may occur, but magnitude does not exceed 2^n
+    if (sum == 33) {
+        if (m ^ n ^ (m ^ n) < 0) return -5;
+        return a * b;
+    }
+    // fallback to slow method for case `sum == 32`
+    // hardware support is required for accurate and speedy detection,
+    // see "Integer Multipliers with Overflow Detection by Gok" et al.
+    // p_n and p_{n-1} are not available without space extension
+    // and wrapping can not store the necessary information.
+    {
+        @setCold(true);
+        const min = math.minInt(ST);
+        var res: ST = a *% b;
+        if ((a < 0 and b == min) or (a != 0 and res / a != b))
+            return -5;
+        return @truncate(ST, res);
+    }
 }
 
 // assume target can represent 2*bitWidth of a and b in register
-fn mulvXi_genericFast(comptime ST: type) fn (a: ST, b: ST) callconv(.C) ST {
-    return struct {
-        fn f(a: ST, b: ST) callconv(.C) ST {
-            const EST = switch (ST) {
-                i32 => i64,
-                i64 => i128,
-                i128 => i256,
-                else => unreachable,
-            };
-            const min = math.minInt(ST);
-            const max = math.maxInt(ST);
-            var res: ST = @as(EST, a) * @as(EST, b);
-            if (res < min or max < res) return -5;
-            return @truncate(ST, res);
-        }
-    }.f;
+inline fn mulvXi_genericFast(comptime ST: type, a: ST, b: ST) ST {
+    @setRuntimeSafety(builtin.is_test);
+    const EST = switch (ST) {
+        i32 => i64,
+        i64 => i128,
+        i128 => i256,
+        else => unreachable,
+    };
+    const bitsize: u32 = @bitSizeOf(ST);
+    const max = math.maxInt(ST); // = 0b0xx..xx to ignore sign bit
+    var res: ST = @as(EST, a) * @as(EST, b);
+    //invariant: -2^{bitwidth(ST)} <= res <= 2^{bitwidth(ST)-1}
+    //=> sign bit is irrelevant in high
+    const high: ST = @truncate(ST, res >> bitsize);
+    const low: ST = @truncate(ST, res);
+
+    if ((high & max) > 0)
+        return -5;
+    //slower: if (res < min or max < res) return -5;
+    return low;
 }
 
-// TODO comptime logic for correct selection
-pub const __mulvsi3 = mulvXi_genericApprox(i32);
-pub const __mulvdi3 = mulvXi_genericApprox(i64);
-pub const __mulvti3 = mulvXi_genericApprox(i128);
+pub fn __mulvsi3(a: i32, b: i32) callconv(.C) i32 {
+    if (@bitSizeOf(i32) <= usize) {
+        return mulvXi_genericFast(i32, a, b);
+    } else {
+        return mulvXi_genericSmall(i32, a, b);
+    }
+}
 
-pub const mulvsi_genericSmall = mulvXi_genericSmall(i32);
-pub const mulvdi_genericSmall = mulvXi_genericSmall(i64);
-pub const mulvti_genericSmall = mulvXi_genericSmall(i128);
+pub fn __mulvdi3(a: i64, b: i64) callconv(.C) i64 {
+    if (@bitSizeOf(i64) <= usize) {
+        return mulvXi_genericFast(i64, a, b);
+    } else {
+        return mulvXi_genericSmall(i64, a, b);
+    }
+}
 
-// TODO comptime logic to ensure register size available on target
-pub const mulvsi3genFast = mulvXi_genericFast(i32);
-pub const mulvdi3genFast = mulvXi_genericFast(i64);
-pub const mulvti3genFast = mulvXi_genericFast(i128);
+pub fn __mulvti3(a: i128, b: i128) callconv(.C) i128 {
+    if (@bitSizeOf(i128) <= usize) {
+        return mulvXi_genericFast(i128, a, b);
+    } else {
+        return mulvXi_genericSmall(i128, a, b);
+    }
+}
